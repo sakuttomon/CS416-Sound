31afda51c75dc38a669680bc4a4ab2b228bbe43a
Irvin Lu <ilu@pdx.edu>
2024-10-08 17:52:41 -0700

    Set up variables for sine wave

--- code/clipped/clipped.py
+++ code/clipped/clipped.py
@@ -1,3 +1,19 @@
 import numpy as np
 from scipy.io.wavfile import write
 import sounddevice
+
+class SineWaveWAV:
+    def __init__(self):
+        """
+        Instantiate all class variables following provided specification on the sine wave to generate.
+        """
+        # Specifications for sine wave:
+        # Channels per frame: 1 (mono)
+        # Sample Format: 16 bit signed (values in the range -32767..32767)
+        self.amplitude = 8192           # 1/4 maximum possible 16-bit amplitude (values in the range -8192..8192)
+        self.frequency = 440            # Hz, cycles per second
+        self.duration = 1               # second
+        self.sample_rate = 48000        # samples per second
+
+if __name__ == "__main__":
+    wav = SineWaveWAV()
