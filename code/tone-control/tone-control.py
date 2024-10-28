8e8da862b30b5933a782abb6cc674cf0508b7f43
Irvin Lu <ilu@pdx.edu>
2024-10-28 14:52:54 -0700

    Understanding assignment: Docstring describing expected workflow

--- code/tone-control/tone-control.py
+++ code/tone-control/tone-control.py
@@ -15,6 +15,26 @@ def loadWAV(file):
     sample_rate, audio_data = wavfile.read(file)
     return sample_rate, audio_data
 
+def toneEqualizer():
+    """
+    Adjust the tone of an audio input, using FFT to measure sound energy across a
+    given window size. 
+    
+    Within each window, frequencies are organized into low, mid, and high level bands. 
+    For each band level, a tone filter scales the frequencies to the overall **average** 
+    band energy of the window. The "speed" of adjusting the tone filters is determined by 
+    the window size (how many samples to adjust at a time), and the number of samples to 
+    move for the next FFT window.
+
+    Args:
+        audio_data (np.array): Array containing data read from the WAV file.
+        sample_rate (int): Number of samples per second.
+        window_size (int): Number of samples in each window to apply FFT on.
+        window_move (int): Number of samples to overlap between windows, controlling the frequentness of adjustments.
+
+    Returns:
+        adjusted_audio (np.array): Array containing audio data with tone adjustments applied.
+    """
 
 if __name__ == "__main__":
     sample_rate, audio_data = loadWAV('sine.wav')
