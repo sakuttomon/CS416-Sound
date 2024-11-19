07c64505c5d1ebad1a38f4990dc920a917f691c6
Irvin Lu <ilu@pdx.edu>
2024-11-18 21:49:48 -0800

    Add generate basic waveform function

--- code/chiptune-synthesizer/chiptune-synthesizer.py
+++ code/chiptune-synthesizer/chiptune-synthesizer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sounddevice as sd
+from scipy import signal
 from scipy.io.wavfile import write
 import pretty_midi # Currnetly incompatible with Python 3.12: https://github.com/craffel/pretty-midi/pull/252
 import os
@@ -25,6 +26,16 @@ class MidiToChiptune:
         self.time_signatures = self.data.time_signature_changes
         self.key_signatures = self.data.key_signature_changes
     
+    def midiNoteToFrequency(self, midi_note):
+        """
+        Converts a MIDI note number to its corresponding frequency value.
+
+        Args:
+            midi_node (int): The pitch from a MIDI note to calculate a frequency from.
+        """
+        # Baseline key A4 is represented as MIDI note 69
+        return 440.0 * (2 ** ((midi_note - 69) / 12))
+    
     def printMidiInfo(self):
         """
         Prints all the MIDI information, listing all instruments involved with the number of notes in each.
@@ -41,8 +52,47 @@ class MidiToChiptune:
         print('\nInstruments:')
         for instrument in self.instruments:
             print(f"{instrument.name} has {len(instrument.notes)} notes")
+            # for note in instrument.notes:
+            #     print(note.pitch)
+            #     print(f"{self.midiNoteToFrequency(note.pitch)}")
         
         print("--------------------")
+    
+    def generateWave(self, frequency, duration, waveform='square', volume=0.5):
+        """
+        Generates a waveform of the specified type for a given frequency. This generation imitates the
+        chiptune effect by synthesizing a basic waveform using a note's pitch from the input MIDI.
+
+        Args:
+            frequency (float): Frequency (Hz) of a note pitch to generate a waveform with.
+            duration (float): Duration of the waveform in seconds. Determined by the note's start and end times.
+            waveform (str): The type of basic waveform to generate for a chiptune effect.
+            volume (float): Volume scaling factor (0.0 to 1.0).
+
+        Returns:
+            np.ndarray: The generated waveform as a numpy array.
+
+        Raises:
+            Exception: If the specified waveform type is unsupported.
+        """
+        time = np.linspace(0, duration, int(SAMPLE_RATE * duration), endpoint=False)
+
+        if waveform == 'square':
+            # Defined as sign function of sinusoid: https://en.wikipedia.org/wiki/Square_wave
+            return volume * np.sign(np.sin(2 * np.pi * frequency * time)) 
+        
+        elif waveform == 'sawtooth':
+            return volume * signal.sawtooth(2 * np.pi * frequency * time)
+        
+        elif waveform == 'triangle':
+            # Width of 0.5 gives a a triangle wave:
+            # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.sawtooth.html
+            return volume * signal.sawtooth(2 * np.pi * frequency * time, width=0.5) 
+        
+        elif waveform == 'sine':
+            return volume * np.sin(2 * np.pi * frequency * time)
+
+        raise Exception(f"{waveform} is not a supported waveform to generate.")
 
 if __name__ == "__main__":
     synth = MidiToChiptune("Kirby's Return to Dreamland - Channel Menu.mid")
